ab0033eeaddb1626b41e1f42b94085b90b49ff61
Joshua Ngugi <joshuamwaurangugi@gmail.com>
2025-09-16 23:53:21 +0300

    Add basic UI

--- lib/add_item_screen.dart
+++ lib/add_item_screen.dart
@@ -12,6 +12,60 @@ class AddItemScreen extends StatefulWidget {
 class _AddItemScreenState extends State<AddItemScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Add Item')),
+      body: Column(
+        children: [
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: const InputDecoration(
+                      labelText: '*Indicates required fields',
+                    ),
+                  ),
+                  TextField(
+                    decoration: const InputDecoration(labelText: 'Item Title*'),
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Enter a title for the item',
+                    ),
+                  ),
+                  TextField(
+                    decoration: const InputDecoration(labelText: 'Description'),
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Add description',
+                    ),
+                    maxLines: 5,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Add Item'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/item_list_screen.dart
+++ lib/item_list_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lister/add_item_screen.dart';
+import 'package:lister/model/item.dart';
 
 class ItemListScreen extends StatefulWidget {
   const ItemListScreen({super.key});
@@ -10,8 +12,51 @@ class ItemListScreen extends StatefulWidget {
 }
 
 class _ItemListScreenState extends State<ItemListScreen> {
+  final List<Item> items = [];
+
+  void _addItem(Item item) {
+    setState(() {
+      items.add(item);
+    });
+  }
+
+  void _deleteItem(int index) {
+    setState(() {
+      items.removeAt(index);
+    });
+  }
+
+  Future<void> _navigateToAddItemScreen() async {
+    final newItem = await Navigator.pushNamed(context, AddItemScreen.routeName);
+    if (newItem != null && newItem is Item) {
+      _addItem(newItem);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Test App')),
+      body: items.isEmpty
+          ? const Center(child: Text('No items added yet'))
+          : ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                final item = items[index];
+                return ListTile(
+                  title: Text(item.title),
+                  subtitle: Text(item.description),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () => _deleteItem(index),
+                  ),
+                );
+              },
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _navigateToAddItemScreen,
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
